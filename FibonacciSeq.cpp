414f3c3c9228287857ff76507eab7901489a4893
enahasan <enamulhasan241@gmail.com>
2025-06-08 00:46:06 +0600

    Fibonacci sequence

--- FibonacciSeq.cpp
+++ FibonacciSeq.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 // This code implements the Fibonacci sequence using recursion
-// The Fibonacci sequence is defined as follows:
-int Fib(int n){
-    if (n <= 1){
-        return n;
+int Fib(int n){ // Function to calculate the nth Fibonacci number
+    if (n <= 1){ // Base case: if n is 0 or 1, return n
+        return n; 
     } 
-    else {    return Fib(n - 1) + Fib(n - 2);
+    else {    return Fib(n - 1) + Fib(n - 2); // Recursive case: return the sum of the two preceding Fibonacci numbers
 } }
-
-int main(){
+// This code prints the Fibonacci sequence up to n numbers
+int main(){ 
     int n;
     cout << "Enter how many Fibonacci numbers you want: ";
     cin >> n;
 
     cout << "Fibonacci sequence up to " << n << " numbers:" << endl;
-    for (int i = 0; i < n; i++){
-        cout << Fib(i) << " "; 
+    for (int i = 0; i < n; i++){ // Loop from 0 to n-1
+        cout << Fib(i) << " ";  // Call the Fib function to get the ith Fibonacci number and print it
     }
     cout << endl;
 
